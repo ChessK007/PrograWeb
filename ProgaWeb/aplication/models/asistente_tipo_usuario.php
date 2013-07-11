3fcf79b44cecb7fe97aeed15e997a5634d3c5696
AlcantarWen <alcantarwendolin@gmail.com>
2013-07-11 08:56:43 -0500

    Actualizacion

--- ProgaWeb/aplication/models/asistente_tipo_usuario.php
+++ ProgaWeb/aplication/models/asistente_tipo_usuario.php
@@ -1,35 +1,37 @@
 <?php
 
-Class Asistente_tipo_usuario {
+Class Asistente_tipo_usuario{
+            
+            private $id_asistente_tipo_usuario;
+            private $id_asistente;
+            private $id_tipo_usuario;
+            
+            
+            
+            function get_id_asistente_tipo_usuario(){
+                return $this->id_asistente_tipo_usuario;
+            }
+            function  set_id_asistente_tipo_usuario($id_asistente_tipo_usuario){
+                $this->id_asistente_tipo_usuario=$id_asistente_tipo_usuario;
+            }
+            
+            public function get_id_asistente(){
+                return $this->id_asistente;
+            }
+            
+            function setid_asistente($id_asistente){
+                $this->id_asistente=$id_asistente;
+            }
+            
+            function getid_tipo_usuario(){
+                return $this->id_tipo_usuario;
+            }
+            
+            function setid_tipo_usuario($id_tipo_usuario){
+                $this->id_tipo_usuario=$id_tipo_usuario;
+            }
 
-    private $id_asistente_tipo_usuario;
-    private $id_asistente;
-    private $id_tipo_usuario;
-
-    Public function get_id_asistente_tipo_usuario() {
-        return $this->id_asistente_tipo_usuario;
-    }
-
-    Public function set_id_asistente_tipo_usuario($id_asistente_tipo_usuario) {
-        $this->id_asistente_tipo_usuario = $id_asistente_tipo_usuario;
-    }
-
-    Public function get_id_asistente() {
-        return $this->id_asistente;
-    }
-
-    Public function set_id_asistente($id_asistente) {
-        $this->id_asistente = $id_asistente;
-    }
-
-    Public function get_id_tipo_usuario() {
-        return $this->id_tipo_usuario;
-    }
-
-    Public function set_id_tipo_usuario($id_tipo_usuario) {
-        $this->id_tipo_usuario = $id_tipo_usuario;
-    }
-
-}
+              
+        }
 
 ?>
