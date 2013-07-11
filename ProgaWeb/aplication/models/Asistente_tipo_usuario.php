859d4324647fca2f61a3638f3968e02d7c9270bd
AlcantarWen <alcantarwendolin@gmail.com>
2013-07-10 23:04:08 -0500

    archivos php

---
+++ ProgaWeb/aplication/models/Asistente_tipo_usuario.php
@@ -0,0 +1,34 @@
+        <? PHP
+		Class Asistente_tipo_usuario{
+		
+            private id_asistente_tipo_usuario;
+            private id_asistente;
+            private id_tipo_usuario;
+            
+            Public get_id_asistente_tipo_usuario(){
+                return $this->id_asistente_tipo_usuario;
+            }
+            
+            Public set_id_asistente_tipo_usuario($valor){
+                $this->id_asistente_tipo_usuario=$valor;
+            }
+            
+            Public get_id_asistente(){
+                return $this->id_asistente;
+            }
+            
+            Public set_id_asistente($valor){
+                $this->id_asistente=$valor;
+            }
+            
+            Public get_id_tipo_usuario(){
+                return $this->id_tipo_usuario;
+            }
+            
+            Public set_id_tipo_usuario($valor){
+                $this->id_tipo_usuario=$valor;
+            }
+
+            
+        }
+		/>
---
+++ ProgaWeb/aplication/models/tipo_usuario.php
@@ -0,0 +1,24 @@
+<? PHP
+	Class Tipo_usuario(){
+		private id_tipo_usuario; 
+		private tipo;
+
+	  public get_id_tipo_usuario(){
+	  return $this->id_tipo_usuario;
+	  }
+	  
+	  public set_id_tipo_usuario($valor){
+	  $this->id_tipo_usuario=$valor;
+	  }
+	  
+	  public get_tipo(){
+	  return $this->tipo;
+	  }
+	 
+	 public set_tipo(){
+	 $this->tipo=$valor;
+	 }
+
+	}
+	
+?>	
