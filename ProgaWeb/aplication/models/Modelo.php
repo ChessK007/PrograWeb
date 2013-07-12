0ed98f0575c6055f992942be493736074a376548
HectorMCP <hector.9107@hotmail.com>
2013-07-12 09:15:07 -0500

    Modelo

---
+++ ProgaWeb/aplication/models/Modelo.php
@@ -0,0 +1,16 @@
+<?php
+   class Modelo{
+           private $db;
+           function Modelo(){
+                      $this->db = ADONewConnection('mysql');
+                      $this->db->debug = true;
+                      $this->db->Connect('localhost', 'root', '', 'db_eventos');
+           }
+
+           public function consulta_datos(){
+
+                      $rs = $this->db->Execute('SELECT * from'.$this->nombre);
+                      return $rs;
+           }
+   }
+?>
