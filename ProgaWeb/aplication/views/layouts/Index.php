5db9971dad4966810a5d69b01d96a724785fc4b2
HectorMCP <hector.9107@hotmail.com>
2013-07-16 07:54:46 -0500

    Hector

---
+++ ProgaWeb/aplication/views/layouts/Index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="UTF-8">
+<title>Index</title>
+<link href="views/bootstrap/css/bootstrap.css" rel="stylesheet"></link>
+<link href="views/bootstrap/css/clases.css" rel="stylesheet"></link>
+</head>
+
+<body class="fondo">
+   <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container fondo">
+                    
+                    <div class="nav-collapse">
+                        <ul class="nav nav-list">
+                            
+							<li class="divider-vertical">Usuarios</li>
+                            <li class="divider-vertical"><a href="#">Evento</a></li>
+                            <li class="divider-vertical"><a href="#">Actividad</a></li>
+                            <li class="divider-vertical"><a href="#">Tipo Usuario</a></li>
+                            <li class="divider-vertical"><a href="#">Eventos Tipo Usuario</a></li>
+                            <li class="divider-vertical"><a href="#">Asistentes Tipos Usuarios</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
+</body>
+</html>
