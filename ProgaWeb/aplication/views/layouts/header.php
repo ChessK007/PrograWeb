ea8594bf57c03a4119e8cbe51b638e30ae8c8919
abraham5590 <dragon_maximus_55@hotmail.com>
2013-07-17 00:17:09 -0500

    jonathan

--- ProgaWeb/aplication/views/layouts/header.php
+++ ProgaWeb/aplication/views/layouts/header.php
@@ -1,4 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">        
 
-hola
-?>
+    </head>
+    <body background="../bootstrap/img/10753.jpg">
+        
+        <div class="container">
+            <div> <img src="../img/top_bg1-8.jpg"></div>
+            
+            <div class="navbar">
+                <div class="navbar-inner">
+                    <div class="container">
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
+
+                        <a class="brand" href="#">Eventos</a>
+                        <a class="brand" href="#">Actividades</a>
+                        <a class="brand" href="#">Temas</a>
+                        <a class="brand" href="#">Actividades</a>
+                        <a class="brand" href="#">Tipos</a>
+                        <a class="brand" href="#">Instructores</a>
+                        <a class="brand" href="#"></a>
+                        <a class="brand" href="#"></a>
+                        <a class="brand" href="#"></a>
+                        
+
+                        <div class="nav-collapse collapse">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div><!--Contenedor-->
+
+
+        
+            
+            
+        
