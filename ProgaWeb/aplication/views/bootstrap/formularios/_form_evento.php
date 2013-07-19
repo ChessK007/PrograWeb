1e7031d315fb803c99b8aa567f375e39eb9cef0f
abraham5590 <dragon_maximus_55@hotmail.com>
2013-07-19 00:37:34 -0500

    jonathan

--- ProgaWeb/aplication/views/bootstrap/formularios/_form_evento.php
+++ ProgaWeb/aplication/views/bootstrap/formularios/_form_evento.php
@@ -4,36 +4,33 @@ include('../../layouts/navegador.php');
 ?>
 <div class="container">
     <center><h1>Ingreso de Datos del Evento</h1></center>
-    <div class="span3">
-        <form name="form1" class="well" action="#" method="get">
-            <label for="nombre">Nombre</label>
-            <input id="nombre" type="text" class="input-medium" size="20" maxlength="50" required="required"/>
-            <label for="lugar">Lugar</label>
-            <input id="lugar" type="text" class="input-medium" size="20" maxlength="50" required="required"/>
-        </form>
-    </div>
-   
+    <div class="row">
+        <form name="form1" action="#" method="get">
+            <div class="span3">            
+                <label for="nombre">Nombre</label>
+                <input id="nombre" type="text" class="input-medium" size="20" maxlength="50" required="required"/>
+                <label for="lugar">Lugar</label>
+                <input id="lugar" type="text" class="input-medium" size="20" maxlength="50" required="required"/>        
+            </div>
 
-    <div class="span3">
-        <form class="well" name="form1" action="#" method="get">
-            <label for="f_inicio">Fecha de Inicio</label>
-            <input id="f_inicio" type="date" class="input-medium" size="20" maxlength="50" required="required"/>
-            <label for="f_fin">Fecha de Fin</label>
-            <input id="f_fin" type="date" class="input-medium" size="20" maxlength="50" required="required"/> 
-        </form>
-    </div>  
-   
+            <div class="span3">        
+                <label for="f_inicio">Fecha de Inicio</label>
+                <input id="f_inicio" type="date" class="input-medium" size="20" maxlength="50" required="required"/>
+                <label for="f_fin">Fecha de Fin</label>
+                <input id="f_fin" type="date" class="input-medium" size="20" maxlength="50" required="required"/> 
+            </div>  
 
-    <div class="span5">
-        <form class="well" name="form1" action="#" method="get">
-            <label for="descipcion">Descripci&oacute;n</label>
-            <textarea id="descripcion" type="text" class="input-xlarge" size="20" maxlength="50" required="required"></textarea>
-            <label for="contacto">Contacto</label>
-            <input id="contacto" type="text" class="input-medium" size="20" maxlength="50" required="required"/>
-            <input type="submit" class="btn btn-primary" name="Capturar">
+
+            <div class="span5">
+                <label for="descipcion">Descripci&oacute;n</label>
+                <textarea id="descripcion" type="text" class="input-xxlarge" size="20" maxlength="50" required="required"></textarea>
+                <label for="contacto">Contacto</label>
+                <input id="contacto" type="text" class="input-medium" size="20" maxlength="50" required="required"/>
+                <input type="submit" class="btn btn-primary" name="Capturar">        
+            </div>
         </form>
-    </div>
-</div>
+    </div><!--row-->
+</div><!--container-->
 <?php
 include('../../layouts/footer.php');
 ?>
--- ProgaWeb/aplication/views/bootstrap/formularios/_form_usuarios.php
+++ ProgaWeb/aplication/views/bootstrap/formularios/_form_usuarios.php
@@ -2,41 +2,42 @@
 include('../../layouts/header.php');
 include('../../layouts/navegador.php');
 ?>
+
 <div class="container">
 
     <center><h1>Ingreso de Datos de Usuario</h1></center>
 
     <div class="row">
+        <div class="span12">
+            <form name="form4" action="#" method="get">
+                <div class="span4">
+                    <label for="nombre">Nombre</label>
+                    <input id="nombre" type="text" size="20" maxlength="50" required="required"/><br>
+                    <label for="A.Paterno">Apellido Paterno</label>
+                    <input id="A.Paterno" type="text" size="20" maxlength="50" required="required"/><br>
+                    <label for="A.Materno">Apellido Materno</label>
+                    <input id="A.Materno" type="text" size="20" maxlength="50" required="required"/><br>            
+                    <label for="control">N&uacute;mero de Control</label>
+                    <input id="control" type="text" maxlength="10" required="required"/>
+                </div>
 
-        <div class="span4">
-            <form name="form4" class="well" action="#" method="get">
-                <label for="nombre">Nombre</label>
-                <input id="nombre" type="text" size="20" maxlength="50" required="required"/><br>
-                <label for="A.Paterno">Apellido Paterno</label>
-                <input id="A.Paterno" type="text" size="20" maxlength="50" required="required"/><br>
-                <label for="A.Materno">Apellido Materno</label>
-                <input id="A.Materno" type="text" size="20" maxlength="50" required="required"/><br>            
-                <label for="control">N&uacute;mero de Control</label>
-                <input id="control" type="text" maxlength="10" required="required"/>
-            </form>
+                <div class="span1"></div>
+
+                <div class="span4">                   
+                        Genero
+                        <input name="sexo" type="radio" value="Masculino" checked="checked"/>Hombre
+                        <input name="sexo" type="radio" value="Femenino"/>Mujer
+                        <label for="edad">Edad</label>
+                        <input id="edad" type="number" maxlength="2" required="required"/>
+                        <label for="email">Email</label>
+                        <input id="email" type="email" maxlength="50" required="required"/>
+                        <label for="n_control">RFC</label>
+                        <input id="n_control" type="text" maxlength="13" required="required"/>
+                        <input type="submit" class="btn btn-primary" value="Capturar"/>
+
+                </div> 
+            </form>   
         </div>
-
-        <div class="span1"></div>
-
-        <div class="span4">
-            <form class="well" name="form4" action="#" method="get">
-                Genero
-                <input name="sexo" type="radio" value="Masculino" checked="checked"/>Hombre
-                <input name="sexo" type="radio" value="Femenino"/>Mujer
-                <label for="edad">Edad</label>
-                <input id="edad" type="number" maxlength="2" required="required"/>
-                <label for="email">Email</label>
-                <input id="email" type="email" maxlength="50" required="required"/>
-                <label for="n_control">RFC</label>
-                <input id="n_control" type="text" maxlength="13" required="required"/>
-                <input type="submit" class="btn btn-primary" value="Capturar"/>
-            </form>
-        </div>        
     </div><!--row-->
 </div><!--container-->
 <?php
